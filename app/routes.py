0f14c10deb02d1530aae09177efa5272c736a5cf
Camille <camelogithub@gmail.com>
2024-07-25 20:21:24 -0300

    first commit

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from app import app
+from flask import render_template, request
+
+@app.route("/")
+@app.route("/index", defaults={"nome":"Psita"})
+@app.route("/index/<nome>/<profissao>/<canal>")
+def index(nome,profissao,canal):
+    dados = {"profissao": profissao, "canal": canal}
+    return render_template("index.html", nome=nome, dados=dados)
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/autenticar", methods=['POST'])
+def autenticar():
+    user = request.form.get("user")
+    password = request.form.get("password")
+    return "usuario: {} e senha {}".format(user, password)
